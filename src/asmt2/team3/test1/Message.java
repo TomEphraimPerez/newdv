79835675e5ed4550a162621eb0515803c7d604d9
Jessica Kim <123612585+jessicaaakimmm@users.noreply.github.com>
2023-05-11 19:31:14 -0700

    moved functions around -Jess (done)
    just to be safe lol

--- src/asmt2/team3/test1/Message.java
+++ src/asmt2/team3/test1/Message.java
@@ -1,8 +1,8 @@
 package asmt2.team3.test1;
 
-import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
+import java.io.Serializable;
 
 public class Message implements Serializable{
 
